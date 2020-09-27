e1504ebcdcd73bf5ced3409f0dba82d883af8323
TadashiYamazaki <tymz1012@gmail.com>
2020-09-27 13:36:55 +0900

    rss対応

---
+++ next-rss.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteTitle: "yama3ki's web site",
+    siteDescription: "yama3ki's web site rss feed",
+    siteLanguage: 'ja',
+    siteCopyright: '©Tadashi Yamazaki',
+    siteUrl: 'https://yama3ki.work',
+}
